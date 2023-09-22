10e737df0288a370f7d76bd96b0c87c5fc6f1909
BGal23 <131159103+BGal23@users.noreply.github.com>
2023-09-22 12:30:44 +0200

    first task finished

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const btnStart = document.querySelector("[data-start]");
+const btnStop = document.querySelector("[data-stop]");
+const background = document.querySelector("body")
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const startColors = () => {
+    timerId = setInterval(() => {
+        background.style.backgroundColor = getRandomHexColor()
+    },1000);
+
+    btnStart.setAttribute("disabled", "");
+    btnStop.removeAttribute("disabled", "");
+
+    btnStart.removeEventListener("click", startColors);
+    btnStop.addEventListener("click", stopColors);
+    return
+}
+
+const stopColors = () => {
+    clearInterval(timerId)
+
+    btnStart.removeAttribute("disabled", "");
+    btnStop.setAttribute("disabled", "");
+
+    btnStop.removeEventListener("click", stopColors);
+    btnStart.addEventListener("click", startColors);
+    return
+}
+
+btnStart.addEventListener("click", startColors)
