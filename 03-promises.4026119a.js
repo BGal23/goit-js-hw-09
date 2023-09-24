2d9914bfc444eeaf51b8b83ee8b9f68b38cca9ee
BGal23 <131159103+BGal23@users.noreply.github.com>
2023-09-24 16:13:42 +0000

    Deploying to gh-pages from @ BGal23/goit-js-hw-09@bb61fa1bfe4055537a9c6f922d7598757809cfb2 🚀

---
+++ 03-promises.4026119a.js
@@ -0,0 +1,2 @@
+const e=document.querySelectorAll("input");document.querySelector("button").addEventListener("click",(()=>{setTimeout((()=>{setInterval((()=>{console.log(e[0].value-e[1].value)}),e[1].value)}),e[0].value-e[1].value)}));
+//# sourceMappingURL=03-promises.4026119a.js.map
