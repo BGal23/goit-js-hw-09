bb61fa1bfe4055537a9c6f922d7598757809cfb2
BGal23 <131159103+BGal23@users.noreply.github.com>
2023-09-24 18:13:11 +0200

    f

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,22 @@
-function createPromise(position, delay) {
+const input = document.querySelectorAll("input")
+const btnCreate = document.querySelector("button")
+
+const createPromise = (position, delay) => {
   const shouldResolve = Math.random() > 0.3;
+  console.log(shouldResolve)
   if (shouldResolve) {
     // Fulfill
   } else {
     // Reject
   }
 }
+
+const newTask = () => {
+  setTimeout(() => {
+    setInterval(() => {
+      console.log(input[0].value - input[1].value)
+    }, input[1].value)
+  }, input[0].value - input[1].value)
+}
+
+btnCreate.addEventListener("click", newTask)
