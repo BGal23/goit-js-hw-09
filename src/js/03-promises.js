dc3414bb4f97bc9df82700a86c2c6c10b2400289
BGal23 <131159103+BGal23@users.noreply.github.com>
2023-09-25 23:44:01 +0200

    have corect third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,20 +1,30 @@
 const input = document.querySelectorAll("input")
 const btnCreate = document.querySelector("button")
+let timerId;
 
 const createPromise = (position, delay) => {
   const shouldResolve = Math.random() > 0.3;
-  console.log(shouldResolve)
   if (shouldResolve) {
     // Fulfill
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`)
   } else {
     // Reject
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`)
   }
 }
 
-const newTask = () => {
+const newTask = (event) => {
+  event.preventDefault()
+  let meter = 1;
+  let delay = input[0].value*1;
   setTimeout(() => {
-    setInterval(() => {
-      console.log(input[0].value - input[1].value)
+    timerId = setInterval(() => {
+      createPromise(meter, delay)
+      meter++;
+      delay = delay + input[1].value*1
+      if (meter - 1 >= input[2].value) {
+        clearInterval(timerId)
+      }
     }, input[1].value)
   }, input[0].value - input[1].value)
 }
