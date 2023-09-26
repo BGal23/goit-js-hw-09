1f58da40f97cf1e8c2e36c322fb455f83effca3c
BGal23 <131159103+BGal23@users.noreply.github.com>
2023-09-26 18:32:13 +0000

    Deploying to gh-pages from @ BGal23/goit-js-hw-09@7a174791fdf4a1ce75c4f862b14811d1239a06be 🚀

--- 03-promises.5fd6540a.js
+++
@@ -1,2 +0,0 @@
-!function(){var e,n=document.querySelectorAll("input"),t=document.querySelector("button");t.addEventListener("click",(function(t){t.preventDefault();var o=1,c=1*n[0].value;setTimeout((function(){e=setInterval((function(){!function(e,n){Math.random()>.3?console.log("✅ Fulfilled promise ".concat(e," in ").concat(n,"ms")):console.log("❌ Rejected promise ".concat(e," in ").concat(n,"ms"))}(o,c),o++,c+=1*n[1].value,o-1>=n[2].value&&clearInterval(e)}),n[1].value)}),n[0].value-n[1].value)}))}();
-//# sourceMappingURL=03-promises.5fd6540a.js.map
